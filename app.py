c0da0f181a4a3d023c4d270e5441a5af8f19ffb7
yutakumagaya <s1f102101604@iniad.org>
2022-07-03 22:05:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ app = Flask(__name__)
 
 kinoko_count = 3
 takenoko_count = 5
-messages=['Hi1','Hi2'] 
+messages = ['Kinoko is wonderful', 'Takenoko is awesome!']
 
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
+
+import re
 
 @app.route('/vote', methods=['POST'])
 def answer():
@@ -28,6 +30,14 @@ def answer():
     for i in range(len(messages)):
         c = 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5'
         m = messages[i]
+         # HTMLインジェクション対策
+        import re
+        m = re.sub(r'&', r'&amp;', m)
+        m = re.sub(r'<', r'&lt;', m)
+        m = re.sub(r'>', r'&gt;', m)
+         # メッセージのフォーマット
+        m = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', m)
+        m = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', m)
         message_html += f'<div class="alert {c}" role="alert">{m}</div>\n'
 
 

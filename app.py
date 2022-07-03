bc3758f6caa6592f28ee969a8ed0221620c319f9
yutakumagaya <s1f102101604@iniad.org>
2022-07-03 21:29:48 +0900

    edit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 kinoko_count = 3
 takenoko_count = 5
-messages = ['Kinoko is wonderful!','Takenoko is awesome!']
+messages=['Hi1','Hi2'] 
 
 @app.route('/')
 def top():
@@ -11,8 +11,9 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
-    kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
-    takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
+    # 進捗グラフの更新
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':

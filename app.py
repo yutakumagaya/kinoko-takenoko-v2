157cc1a4317a068eeb8329dc36e06259fea20100
yutakumagaya <s1f102101604@iniad.org>
2022-07-03 21:39:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,24 @@ def answer():
         kinoko_count += 1
     elif request.form.get('item')=='takenoko':
         takenoko_count += 1
+    
+    # python内のカウンタの更新
+    messages.append(request.form.get('message'))
+    if len(messages) > 3:
+        messages.pop(0)
+
+    # vote.htmlに渡す内容を変数message_htmlとしてつくる
+    message_html = ''
+    for i in range(len(messages)):
+        c = 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5'
+        m = messages[i]
+        message_html += f'<div class="alert {c}" role="alert">{m}</div>\n'
+
+
     # 進捗グラフの更新
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html',**vars())
-    
+
 if __name__ == '__main__':
     app.run(debug=True)

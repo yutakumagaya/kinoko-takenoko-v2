de58b292505c7f1b1812e519a45c4dbd758ec689
yutakumagaya <s1f102101604@iniad.org>
2022-07-03 21:34:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,16 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    # 投票による更新
+    global kinoko_count, takenoko_count, messages
+    if request.form.get('item')=='kinoko':
+        kinoko_count += 1
+    elif request.form.get('item')=='takenoko':
+        takenoko_count += 1
     # 進捗グラフの更新
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
-    return render_template('vote.html', **vars())
-
+    return render_template('vote.html',**vars())
+    
 if __name__ == '__main__':
     app.run(debug=True)
